1756eb41a930c5f390973c6e2053ce9f2389b2dc
Riley Shimp <rilryshimp@gmail.com>
2022-01-11 10:55:34 -0800

    Add buttons to change background color
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
   $(".hover").click(function() {
     $(".initial-showing").slideToggle();
-    // $(".initial-hidden").slideToggle();
+  });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
